613a4509a7e48c09b619d3374b8757f3b834e989
unknown <wagner.nick@live.com>
2019-07-18 22:56:40 -0400

    Added better median of 3 and merge sort on arrays

--- QS.cpp
+++ QS.cpp
@@ -5,6 +5,15 @@
 #include <time.h>
 #include <sys/resource.h>
 
+#include <vector>
+#include <future>
+#include <chrono>
+
+/* Coming soon...
+#include "heap.hpp"
+#include "tree.hpp"
+*/
+
 using namespace std;
 
 
@@ -62,6 +71,33 @@ int medOfThree (const string * A, const int a, const int b) {
       return beg;
     } else {
       return mid;
+    }
+  }
+}
+
+/**
+ * Slightly more slimmed down version of the above
+ * Maximum of 3 comparisons ever made (as compared to the other's 5)
+ */
+int medianOfThree (const string * A, const int a, const int b) {
+  int beginning = a, middle = (a + b) / 2, end = b;
+  string first = A[beginning], second = A[middle], third = A[end];
+
+  if (first < second) {          //1, 2, 3 or 1, 3, 2 or 3, 1, 2
+    if (second < third) {
+      return middle;
+    } else if (first < third) {
+      return end;
+    } else {
+      return beginning;
+    }
+  } else {                      //2, 1, 3 or 2, 3, 1 or 3, 2, 1
+    if (first < third) {
+      return beginning;
+    } else if (second < third) {
+      return end;
+    } else {
+      return middle;
     }
   }
 }
@@ -178,7 +214,8 @@ string randomString (const int len) {
   const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopkrstuvwxyz";
   string result = "";
   for (int i = 0; i < len; i++) {
-    result += alphabet[random() % (alphabet.length() - 1)];
+//    result += alphabet[random() % (alphabet.length() - 1)];			Not compatible with Cygwin?
+    result += alphabet[rand() % (alphabet.length() - 1)];
   }
   return result;
 }
@@ -206,21 +243,137 @@ double getCPUTime() {
          ruse.ru_stime.tv_sec + ruse.ru_stime.tv_usec / 1000000.0;
 }
 
+/**
+ * Thread-specific timing function (takes CPU timing per thread)
+ *  - ISSUE: Times seem to be longer than process wide CPU Timer
+ *           for multi-threaded clock-ins using std::async?
+ */
+double getTime () {
+  struct timespec time;
+  clock_gettime(CLOCK_THREAD_CPUTIME_ID, &time);
+//  cout << "Time is " << time.t
+  return time.tv_sec + time.tv_nsec / 1000000000.0;
+}
+
+/**
+ * Multithreaded "run" function, performs a singular variation of quicksort
+ *  - ISSUE: Timings gathered by start/end do NOT coincide with the sequential
+ *           implementation (as in oldRun(...))
+ */
+double handleSort (string * Master, string * Sorted, const int index, const int size,
+                   void (* part) (string *, const int, const int, int &, int &, int (*) (const string *, const int, const int)),
+                   int (* med) (const string *, const int, const int)) {
+  string * copy = new string[size];
+  for (int i = 0; i < size; i++) {
+    copy[i] = Master[i];
+  }
+  double start = getTime();
+//  double t_start = time(NULL);
+//  auto c_start = chrono::steady_clock::now();
+
+  quicksort(copy, 0, size - 1, part, med); 
+
+  double end = getTime();
+//  double t_end = time(NULL);
+//  auto c_end = chrono::steady_clock::now();
+
+//  chrono::duration<double> change = c_end - c_start;
+
+//  double diff = difftime(t_end, t_start);
+
+  cout << "Index " << index / 10 << "." << index % 10 << " time was " << end - start << " seconds.\n";
+
+  for (int i = 0; i < size; i++) {
+    if (copy[i] != Sorted[i]) {
+      cout << "Mismatch at position: " << i << "for array with index: " << index / 10 << "." << index % 10 << endl;
+      exit(1);
+    }
+  }
+  delete copy;
+  return end - start;
+}
+
+/**
+ * Multi-threading implementation of all of the variations of quicksort
+ *  - ISSUE: Timings do NOT line up with the sequential testing implementation
+ *           Off (longer) by about 75% or so
+ */
+/*
+void handleAll (string * Master, const int size) {
+  void (* partArray[]) (string *, const int, const int, int &, int &, int (*) (const string *, const int, const int))
+        = {origPart, triRegionPart, twoRegionPart};
+  const int PART_ARRAY_SIZE = 3;
+  int (* medArray[]) (const string *, const int, const int) = {firMed, midMed, endMed, medOfThree};
+  const int MED_ARRAY_SIZE = 4;
+  vector<future<double>> returns; 
+
+  double start_all = getTime();
+
+  string * QSorted = new string [size];
+  string * AlgSorted = new string [size];
+
+  for (int i = 0; i < size; i++) {
+    QSorted[i] = AlgSorted[i] = Master[i];
+  }
+
+  double start = getCPUTime();
+  sort(AlgSorted, AlgSorted + size);
+  double end = getCPUTime();
+  cout << "First check (sort(...) from <algorithm>) finished sorting in " << end - start << " seconds.\n";
+
+  start = getCPUTime();
+  qsort(QSorted, size, sizeof(string), compare);
+  end = getCPUTime();
+  cout << "Second check (qsort(...) from <stdlib.h>) finished sorting in " << end - start << " seconds.\n";
+
+  for (int i = 0; i < size; i++) {
+    if (QSorted[i] != AlgSorted[i]) {
+      cout << "The checks don't match up...\n";
+      exit(1);
+    }
+  }
+
+  for (int i = 0; i < PART_ARRAY_SIZE; i++) {
+    for (int j = 0; j < MED_ARRAY_SIZE; j++) {
+      returns.push_back(async(handleSort, Master, AlgSorted, (10 * i) + j, size, partArray[i], medArray[j]));
+    }
+  }
+  
+  cout << endl << endl;
+
+  for (int i = 0; i < PART_ARRAY_SIZE; i++) {
+    for (int j = 0; j < MED_ARRAY_SIZE; j++) {
+      cout << "Sort index: " << i << "." << j << " finished in " << returns.at((i * MED_ARRAY_SIZE) + j).get() << " seconds.\n";
+    }
+    cout << endl;
+  }
+  double end_all = getTime();
+
+  cout << "Entire new process takes " << end_all - start_all << " seconds.\n\n";
+
+  delete QSorted;
+  delete AlgSorted;
+}
+*/
+
 //Handles the running of all the different versions of the sorts given the appropriate arrays (A-L, and checks), size,  and the 
 //  (choice) of which block(s) to run
 //No return, but arrays will be modified by reference depending on choice input
 //choice: 1 = original partition, 2 = tri-region partition, 3 = two-region partition, 4 = all partition methods
 void runSorts (string * A, string * B, string * C, string * D, string * E, string * F, string * G,
                string * H, string * I, string * J, string * K, string * L, string * check, string * check2,
+               string * X, string * Y, string * Z,
                const int choice, const int size) {
 
-  double t[30];
+  double t[30], start, stop;
 
   if (choice == 1 || choice == 4) {
     t[0] = getCPUTime();
+    double t1 = getTime();
     quicksort(A, 0, size - 1, origPart, firMed);
+    double t2 = getTime();
     t[1] = getCPUTime();
-    cout << "\nOriginal Partition Method with first element pivot finished sorting in " << t[1] - t[0] << " seconds.\n";
+    cout << "\nOriginal Partition Method with first element pivot finished sorting in " << t[1] - t[0] << " (" << t2 - t1 << ") seconds.\n";
     t[2] = getCPUTime();
     quicksort(B, 0, size - 1, origPart, midMed);
     t[3] = getCPUTime();
@@ -232,7 +385,11 @@ void runSorts (string * A, string * B, string * C, string * D, string * E, strin
     t[6] = getCPUTime();
     quicksort(D, 0, size - 1, origPart, medOfThree);
     t[7] = getCPUTime();
-    cout << "Original Partition Method with median of three pivot finished sorting in " << t[7] - t[6] << " seconds.\n\n";
+    cout << "Original Partition Method with old median of three pivot finished sorting in " << t[7] - t[6] << " seconds.\n";
+    start = getCPUTime();
+    quicksort(X, 0, size - 1, origPart, medianOfThree);
+    stop = getCPUTime();
+    cout << "Original Partition Method with new median of three pivot finished sorting in " << stop - start << " seconds.\n\n";
   }
   if (choice == 2 || choice == 4) {
     t[8] = getCPUTime();
@@ -250,7 +407,11 @@ void runSorts (string * A, string * B, string * C, string * D, string * E, strin
     t[14] = getCPUTime();
     quicksort(H, 0, size - 1, triRegionPart, medOfThree);
     t[15] = getCPUTime();
-    cout << "Tri-Region Partition Method with median of three pivot finished sorting in " << t[15] - t[14] << " seconds.\n\n";
+    cout << "Tri-Region Partition Method with old median of three pivot finished sorting in " << t[15] - t[14] << " seconds.\n";
+    start = getCPUTime();
+    quicksort(Y, 0, size - 1, triRegionPart, medianOfThree);
+    stop = getCPUTime();
+    cout << "Tri-Region Partition Method with new median of three pivot finished sorting in " << stop - start << " seconds.\n\n";
   }
   if (choice == 3 || choice == 4) {
     t[16] = getCPUTime();
@@ -268,7 +429,11 @@ void runSorts (string * A, string * B, string * C, string * D, string * E, strin
     t[22] = getCPUTime();
     quicksort(L, 0, size - 1, twoRegionPart, medOfThree);
     t[23] = getCPUTime();
-    cout << "Two-Region Partition Method with median of three pivot finished sorting in " << t[23] - t[22] << " seconds.\n\n";
+    cout << "Two-Region Partition Method with old median of three pivot finished sorting in " << t[23] - t[22] << " seconds.\n";
+    start = getCPUTime();
+    quicksort(Z, 0, size - 1, twoRegionPart, medOfThree);
+    stop = getCPUTime();
+    cout << "Two-Region Partition Method with new median of three pivot finished sorting in " << stop - start << " seconds.\n\n";
   }
   t[24] = getCPUTime();
   sort(check, check + size);
@@ -286,6 +451,7 @@ void runSorts (string * A, string * B, string * C, string * D, string * E, strin
 //choice: 1 = original partition, 2 = tri-region partition, 3 = two-region partition, 4 = all partition methods
 void checkSorts (string * A, string * B, string * C, string * D, string * E, string * F, string * G,
                  string * H, string * I, string * J, string * K, string * L, string * check, string * check2,
+                 string * X, string * Y, string * Z,
                  const int choice, const int size) {
   cout << endl;
   bool equal = true;
@@ -311,6 +477,10 @@ void checkSorts (string * A, string * B, string * C, string * D, string * E, str
         equal = false;
         cout << "D ";
       }
+      if (X[i] != check[i] || X[i] != check2[i]) {
+        equal = false;
+        cout << "X ";
+      }
     }
     if (choice == 2 || choice == 4) {
       if (E[i] != check[i] || E[i] != check2[i]) {
@@ -329,6 +499,10 @@ void checkSorts (string * A, string * B, string * C, string * D, string * E, str
         equal = false;
         cout << "H ";
       }
+      if (Y[i] != check[i] || Y[i] != check2[i]) {
+        equal = false;
+        cout << "Y ";
+      }
     }
     if (choice == 3 || choice == 4) {
       if (I[i] != check[i] || I[i] != check2[i]) {
@@ -346,6 +520,10 @@ void checkSorts (string * A, string * B, string * C, string * D, string * E, str
       if (L[i] != check[i] || L[i] != check2[i]) {
         equal = false;
         cout << "L ";
+      }
+      if (Z[i] != check[i] || Z[i] != check2[i]) {
+        equal = false;
+        cout << "Z ";
       }
     }
     if (!equal) {
@@ -418,31 +596,211 @@ int sortChoice () {
   return sortChoice();
 }
 
-int main() {
-  sranddev();
-
-//User input for size of arrays, string length, and sort choice
-  int length, size;
-  input(size, length);
+/**
+ * Handles all of the running all variations of quicksort SEQUENTIALLY
+ *  - TODO: streamline into something more modular and perhaps perform
+ *          sorts in batches (no bulk memory allocation as is done below)
+ */
+void oldRun (string * Master, const int size) {
   const int choice = sortChoice();
 
+  double start = getCPUTime();
 //Array memory allocation
   string * A = new string[size], * B = new string[size], * C = new string[size], * D = new string[size],
          * E = new string[size], * F = new string[size], * G = new string[size], * H = new string[size],
-         * I = new string[size], * J = new string[size], * K = new string[size], * L = new string[size];
+         * I = new string[size], * J = new string[size], * K = new string[size], * L = new string[size],
+         * X = new string[size], * Y = new string[size], * Z = new string[size];
 
   string * check = new string[size], * check2 = new string[size];
   
 //Assigning the same strings to all of the arrays for each element
   for (int i = 0; i < size; i++) {
-    A[i] = B[i] = C[i] = D[i] = E[i] = F[i] = G[i] = H[i] = I[i] = J[i] = K[i] = L[i] = check[i] = check2[i] = randomString(length);
+    A[i] = B[i] = C[i] = D[i] = E[i] = F[i] = G[i] = H[i] = I[i] = J[i] = K[i] = L[i] = check[i] = check2[i] = X[i] = Y[i] = Z[i] = Master[i];
   }
 
 //Running and checking the sorts
-  runSorts(A, B, C, D, E, F, G, H, I, J, K, L, check, check2, choice, size);
-  checkSorts(A, B, C, D, E, F, G, H, I, J, K, L, check, check2, choice, size);
+  runSorts(A, B, C, D, E, F, G, H, I, J, K, L, check, check2, X, Y, Z, choice, size);
+  checkSorts(A, B, C, D, E, F, G, H, I, J, K, L, check, check2, X, Y, Z, choice, size);
+  double end = getCPUTime();
 
-//Should deallocate memory for the above arrays, but program ends here so whatever
+  cout << "Entire old process takes " << end - start << " seconds.\n\n";
+
+//deallocate everything
+  delete A; delete B; delete C; delete D; delete E; delete F;
+  delete G; delete H; delete I; delete J; delete K; delete L;
+  delete X; delete Y; delete Z;
+  delete check; delete check2;
+}
+
+/**
+ * Merge sort on an array.
+ *  - Time complexity = O(nlog(n))
+ *  - Space complexity = O(1)
+ *   - Achieved with continuous trading of elements between 2 arrays
+ *  - TODO: Separate this into another code file
+ */
+void MergeSort (string *& master, const int size) {
+  string * A = master;
+  string * temp = new string[size];
+//batch represents the offset between chunks to be compared
+// since the array is treated as having been split down to singular
+// elements logically and is "built up" from the beginning
+  for (int batch = 1; batch < size; batch *= 2) {
+    int section1Start = 0;
+    int section1End = section1Start + batch - 1;
+    int section2Start = section1End + 1;
+    int section2End = section2Start + batch - 1 >= size ? size - 1 : section2Start + batch - 1; //Check <= size - 1
+    int tempi = 0;
+    while (section1Start < size) {
+      while (true) {
+//Copy over remainder of the other subsection of the array
+        if (section1Start > section1End) {
+          while(section2Start <= section2End) {
+            temp[tempi] = A[section2Start];
+            tempi++;
+            section2Start++;
+          }
+          break;
+        } else if (section2Start > section2End) {
+          while (section1Start <= section1End) {
+            temp[tempi] = A[section1Start];
+            tempi++;
+            section1Start++;
+          }
+          break;
+        }
+//Compare the two subsections and move the lower one forward
+        if (A[section1Start] <= A[section2Start]) {
+          temp[tempi] = A[section1Start];
+          section1Start++;
+        } else {
+          temp[tempi] = A[section2Start];
+          section2Start++;
+        }
+        tempi++;
+      }
+//Move on to the next subsections in the same batch
+      section1Start = section2End + 1;
+      if (section1Start + batch - 1 >= size) {
+        section1End = size - 1;   //check <= size - 1
+        section2Start = size;
+        section2End = size - 1;
+      } else {
+        section1End = section1Start + batch - 1;
+        section2Start = section1End + 1;
+        section2End = section2Start + batch - 1 >= size ? size - 1 : section2Start + batch - 1;   //check <= size - 1
+      }
+    }
+/* Printing for debugging
+    cout << "A: " << A << " master: " << master << " temp: " << temp << " batch: " << batch << endl;
+    for (int i = 0; i < size; i++) {
+      cout << "A[" << i << "]: " << A[i] << " temp[" << i << "]: " << temp[i] << endl;
+    }
+*/
+//Swap the arrays so the next round copies into the other array (improves space complexity)
+    string * swap = temp;
+    temp = A;
+    A = swap;
+  }
+//  cout << "A: " << A << " master: " << master << " temp: " << temp << endl;
+//Make sure the right array is returned (this is why master is a reference pointer,
+// otherwise we would have to copy the entire contents of the array here)
+  delete[] temp;
+  if (A != master) {
+    master = A;
+  }
+}
+
+int main() {
+//  sranddev();		Not compatible with Cygwin?
+  srand(time(NULL));
+
+//User input for size of arrays, string length, and sort choice
+  int length, size;
+  input(size, length);
+
+  string * Master = new string[size];
+  for (int i = 0; i < size; i++) {
+    Master[i] = randomString(length);
+  }
+
+  oldRun(Master, size);
+
+// Coming soon...
+//  heap<string, Heap::MAX> h(size);
+//  tree<string> t;
+  string * check = new string[size], * merge_data = new string[size];
+  string * * Master_Copy = new string * [size];
+  for (int i = 0; i < size; i++) {
+    check[i] = merge_data[i] = Master[i];
+    Master_Copy[i] = new string(Master[i]);
+  }
+  sort(check, check + size);
+  double t1, t2, t3;
+  bool flag;
+/* TODO: Separate code files for the lot of these
+  t1 = getCPUTime();
+  for (int i = 0; i < size; i++) {
+    h.push(Master[i]);
+  }
+  t2 = getCPUTime();
+  string * data = h.sort();
+  t3 = getCPUTime();
+
+  cout << "\nHeap sort total time: " << t3 - t1 << " seconds, sort time: " << t3 - t2 << " seconds, load time: " << t2 - t1 << " seconds.\n";
+  flag = true;
+  for (int i = 0; i < size; i++) {
+    if (check[i] != data[i]) {
+      flag = false;
+      break;
+    }
+  }
+  if (flag == true) {
+    cout << "Heap sort passed the check\n\n";
+  }
+
+  t1 = getCPUTime();
+  for (int i = 0; i < size; i++) {
+    t.push(Master_Copy[i]);
+  }
+  t2 = getCPUTime();
+  vector<string *> tree_data = t.sort();
+  t3 = getCPUTime();
+
+  cout << "Tree sort total time: " << t3 - t1 << " seconds, sort time: " << t3 - t2 << " seconds, load time: " << t2 - t1 << " seconds.\n";
+  flag = true;
+  for (int i = 0; i < size; i++) {
+    if (check[i] != *(tree_data[i])) {
+      flag = false;
+      break;
+    }
+  }
+  if (flag == true) {
+    cout << "Tree sort passed the check\n\n";
+  }
+*/
+  t1 = getCPUTime();
+  MergeSort(merge_data, size);
+  t2 = getCPUTime();
+
+  cout << "Merge sort on array finished in " << t2 - t1 << " seconds.\n";
+  flag = true;
+  for (int i = 0; i < size; i++) {
+    if (check[i] != merge_data[i]) {
+      flag = false;
+      cout << i << endl;
+    }
+  }
+  if (flag == true) {
+    cout << "Merge sort passed the check\n\n";
+  } else {
+    cout << "Merge sort failed the check\n\n";
+  }
+//  handleAll(Master, size);    //async implementation needs to be fixed
+
+  delete Master;
+  delete Master_Copy;
+  delete check;
 
   return 0;
 }
